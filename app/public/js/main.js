f841f02a08f35ae44035f6d58870c6891b058d50
Holderness <andersonholderness@gmail.com>
2015-04-19 16:56:09 -0400

    more alignment fixes, unecessary file deletions

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,80 +1,74 @@
 $(document).ready(function() {
     
-    var mainController = new MainController();
+  var mainController = new MainController();
+  mainController.init();
 
-    mainController.init();
 });
 
 
 var MainController = function() {
-    var self = this;
-    
-    self.appEventBus = _.extend({}, Backbone.Events);
-    
-    self.viewEventBus = _.extend({}, Backbone.Events);
-    
-    self.init = function() {
+	var self = this;
 
-	self.chatClient = new ChatClient({ vent: self.appEventBus });
-	self.chatClient.connect();
+	self.appEventBus = _.extend({}, Backbone.Events);
 
-	self.loginModel = new LoginModel();
-	
-	self.containerModel = new ContainerModel({ viewState: new LoginView({vent: self.viewEventBus, model: self.loginModel})});
-	self.containerView = new ContainerView({ model: self.containerModel });
+	self.viewEventBus = _.extend({}, Backbone.Events);
 
-	self.containerView.render();
-    }
+	self.init = function() {
+		self.chatClient = new ChatClient({ vent: self.appEventBus });
+		self.chatClient.connect();
 
-    self.viewEventBus.on("login", function(name) {
-	// socketio login
-	self.chatClient.login(name);
-    });
+		self.loginModel = new LoginModel();
 
-    self.viewEventBus.on("chat", function(chat) {
-	// socketio chat
-	self.chatClient.chat(chat);
-    });
+		self.containerModel = new ContainerModel({ viewState: new LoginView({vent: self.viewEventBus, model: self.loginModel})});
+		self.containerView = new ContainerView({ model: self.containerModel });
 
-    self.appEventBus.on("loginDone", function() {
-	self.homeModel = new HomeModel();
-	self.homeView  = new HomeView({vent: self.viewEventBus, model: self.homeModel });
+		self.containerView.render();
+	};
 
-	self.containerModel.set("viewState", self.homeView);
-    });
+	self.viewEventBus.on("login", function(name) {
+    // socketio login
+    self.chatClient.login(name);
+  });
 
-    self.appEventBus.on("loginNameBad", function(name) {
-	self.loginModel.set("error", "Invalid Name");
-    });
+	self.viewEventBus.on("chat", function(chat) {
+    // socketio chat
+    self.chatClient.chat(chat);
+  });
 
-    self.appEventBus.on("loginNameExists", function(name) {
-	self.loginModel.set("error", "Name already exists");
-    });
-
-    self.appEventBus.on("usersInfo", function(data) {
-	var onlineUsers = self.homeModel.get("onlineUsers");
-
-	var users = _.map(data, function(item) {
-	    return new UserModel({name: item});
+	self.appEventBus.on("loginDone", function() {
+		self.homeModel = new HomeModel();
+		self.homeView  = new HomeView({vent: self.viewEventBus, model: self.homeModel });
+		self.containerModel.set("viewState", self.homeView);
 	});
-	
-	onlineUsers.reset(users);
-    });
 
-    self.appEventBus.on("userJoined", function(username) {
-	self.homeModel.addUser(username);
+	self.appEventBus.on("loginNameBad", function(name) {
+		self.loginModel.set("error", "Invalid Name");
+	});
 
-	self.homeModel.addChat({sender: "", message: username + " joined room." });
-    });
+	self.appEventBus.on("loginNameExists", function(name) {
+		self.loginModel.set("error", "Name already exists");
+	});
 
-    self.appEventBus.on("userLeft", function(username) {
-	self.homeModel.removeUser(username);
+	self.appEventBus.on("usersInfo", function(data) {
+		var onlineUsers = self.homeModel.get("onlineUsers");
+		var users = _.map(data, function(item) {
+			return new UserModel({name: item});
+		});
+		onlineUsers.reset(users);
+	});
 
-	self.homeModel.addChat({sender: "", message: username + " left room." });
-    });
+	self.appEventBus.on("userJoined", function(username) {
+		self.homeModel.addUser(username);
+		self.homeModel.addChat({sender: "", message: username + " joined room." });
+	});
 
-    self.appEventBus.on("chatReceived", function(chat) {
-	self.homeModel.addChat(chat);
-    });
-}
+	self.appEventBus.on("userLeft", function(username) {
+		self.homeModel.removeUser(username);
+		self.homeModel.addChat({sender: "", message: username + " left room." });
+	});
+
+	self.appEventBus.on("chatReceived", function(chat) {
+		self.homeModel.addChat(chat);
+	});
+};
 
